43cfe1426f6f746187cf22a1d75e7434e1e9ce8e
Lala5th <lala5th@gmail.com>
2020-08-05 19:31:23 +0200

    Updated README and expanded test

--- test.cc
+++ test.cc
@@ -2,8 +2,13 @@
 
 int main(){
     NumpyAnalysisManager* man = NumpyAnalysisManager::GetInstance();
-    int id = man->CreateDataset<int,char>("name");
+    int id = man->CreateDataset<int,char>("arr1");
+    int id2 = man->CreateDataset<int,float>("arr2");
     man->AddData<int,char>(id,5,'a');
-    man->AddData(id,7,'b');
+    for(size_t i = 0; i < 1000; i++)
+        man->AddData<int,char>(id,i,'b');
+    man->AddData<int,float>(id2,4,3.5);
+    for(size_t i = 0; i < 1000; i++)
+    man->AddData<int,float>(id2,i,4.7);
     man->WriteData();
 }
